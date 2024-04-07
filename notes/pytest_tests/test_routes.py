b9c2c8009519303e9295fa3e01fceb2151212329
Evgenii Shishkin <tictac4jeka@gmail.com>
2024-04-07 20:42:48 +0800

    new_initial

---
+++ notes/pytest_tests/test_routes.py
@@ -0,0 +1,25 @@
+# test_routes.py
+from http import HTTPStatus
+
+from django.urls import reverse
+import pytest
+
+
+# Указываем в фикстурах встроенный клиент.
+def test_home_availability_for_anonymous_user(client):
+    # Адрес страницы получаем через reverse():
+    url = reverse('notes:home')
+    response = client.get(url)
+    assert response.status_code == HTTPStatus.OK
+
+
+@pytest.mark.parametrize(
+    'name',  # Имя параметра функции.
+    # Значения, которые будут передаваться в name.
+    ('notes:home', 'users:login', 'users:logout', 'users:signup')
+)
+# Указываем имя изменяемого параметра в сигнатуре теста.
+def test_pages_availability_for_anonymous_user(client, name):
+    url = reverse(name)  # Получаем ссылку на нужный адрес.
+    response = client.get(url)  # Выполняем запрос.
+    assert response.status_code == HTTPStatus.OK
