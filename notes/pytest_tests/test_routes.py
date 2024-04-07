f40bfcd08ae3182cf45eb6054840997d6e5f25bd
Evgenii Shishkin <tictac4jeka@gmail.com>
2024-04-07 21:12:50 +0800

    add slug_for_args to test

--- notes/pytest_tests/test_routes.py
+++ notes/pytest_tests/test_routes.py
@@ -40,9 +40,9 @@ def test_pages_availability_for_anonymous_user(client, name):
     ('notes:detail', 'notes:edit', 'notes:delete'),
 )
 def test_pages_availability_for_different_users(
-        parametrized_client, name, note, expected_status
+        parametrized_client, name, slug_for_args, expected_status
 ):
-    url = reverse(name, args=(note.slug,))
+    url = reverse(name, args=slug_for_args)
     response = parametrized_client.get(url)
     assert response.status_code == expected_status
 
